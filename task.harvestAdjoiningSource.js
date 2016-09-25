f2ed56ab3e9335de7cf6e3f89b9f1a6fb8f43a6b
sethten <sethten@users.noreply.github.com>
2016-09-25 15:48:56 -0400

    Fix harvestAdjoiningSource
    Basically, revert c583902, leave b8373fe, add `!flag.room || `

--- task.harvestAdjoiningSource.js
+++ task.harvestAdjoiningSource.js
@@ -2,10 +2,10 @@ var taskHarvestAdjoiningSource = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        var source = Game.rooms.W52N6.find(FIND_SOURCES)[0];
+        var flag = Game.flags['AdjoiningRoomSource'];
         if (creep.carry.energy < creep.carryCapacity) {
-            if (creep.room.name != source.room.name || creep.harvest(source) == ERR_NOT_IN_RANGE)
-                creep.moveTo(source);
+            if (!flag.room || creep.room.name != flag.room.name || creep.harvest(flag.pos.lookFor(LOOK_SOURCES)[0]) == ERR_NOT_IN_RANGE)
+                creep.moveTo(flag);
         } else { // full on energy, start heading back home
             creep.moveTo(Game.rooms.W53N6.controller);
         }
