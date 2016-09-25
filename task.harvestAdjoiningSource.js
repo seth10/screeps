b8373fe6f208c97c6b03b7e23d68c839f27b11b4
sethten <sethten@users.noreply.github.com>
2016-09-25 15:09:11 -0400

    Fix #7
    If creep.carry.energy == creep.carryCapacity, we want the creep to head
    back, not keep harvesting.

--- task.harvestAdjoiningSource.js
+++ task.harvestAdjoiningSource.js
@@ -3,7 +3,7 @@ var taskHarvestAdjoiningSource = {
     /** @param {Creep} creep **/
     run: function(creep) {
         var source = Game.rooms.W52N6.find(FIND_SOURCES)[0];
-        if (creep.carry.energy <= creep.carryCapacity) {
+        if (creep.carry.energy < creep.carryCapacity) {
             if (creep.room.name != source.room.name || creep.harvest(source) == ERR_NOT_IN_RANGE)
                 creep.moveTo(source);
         } else { // full on energy, start heading back home
