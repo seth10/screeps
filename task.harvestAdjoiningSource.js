c58390298945e98b71d5684c0a8ac7d259a2d9ac
sethten <sethten@users.noreply.github.com>
2016-09-25 14:59:19 -0400

    Remove AdjoiningRoomSource flag dependence

--- task.harvestAdjoiningSource.js
+++ task.harvestAdjoiningSource.js
@@ -2,10 +2,10 @@ var taskHarvestAdjoiningSource = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        var flag = Game.flags['AdjoiningRoomSource'];
+        var source = Game.rooms.W52N6.find(FIND_SOURCES)[0];
         if (creep.carry.energy <= creep.carryCapacity) {
-            if (creep.room.name != flag.room.name || creep.harvest(flag.pos.lookFor(LOOK_SOURCES)[0]) == ERR_NOT_IN_RANGE)
-                creep.moveTo(flag);
+            if (creep.room.name != source.room.name || creep.harvest(source) == ERR_NOT_IN_RANGE)
+                creep.moveTo(source);
         } else { // full on energy, start heading back home
             creep.moveTo(Game.rooms.W53N6.controller);
         }
