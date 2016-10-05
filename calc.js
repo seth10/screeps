af25754de670f861eb1c61ee3cf8ba6e0aab2ac2
sethten <sethten@users.noreply.github.com>
2016-10-05 18:32:20 -0400

    Specify controller or spawn for calc
    Creeps may only send one energy per tick through upgradeController, but
    they can transfer all of their energy to a spawn in one tick. If the
    creep is targeting the controller, add carry to the total time taken to
    more accurately calculate efficiency.

--- calc.js
+++ calc.js
@@ -11,6 +11,7 @@
  *   CARRY_CAPACITY = 50
  *   HARVEST_POWER = 2
  *   SOURCE_ENERGY_CAPACITY = 3000 // not used here, but might be useful
+ *   UPGRADE_CONTROLLER_POWER = 1
  *
  * possible improvements:
  *   maybe keep a list if efficiency == best['efficiency']
@@ -22,13 +23,14 @@
  * Find the best set of body parts for a creep. This is intended for a basic harvesting creep with only WORK, CARRY, and MOVE parts.
  * @param {StructureSpawn} spawn - The spawner which will be spawning the creep, used to call spawn.room.energyCapacityAvailable.
  * @param {number} distance - Distance of path betweem source and controller.
+ * @param {boolean} upgradeController - True if the creep will be transfering energy to the room controller (add carry to time) or spawn (dumps all energy in one tick).
  * @returns {Object.<string, number>} best - Information about the best configuration of body parts found.
  * @returns {number} best.efficiency - The total energy mined per trip divided by the ticks to complete that trip.
  * @returns {number} best.work - The number of WORK parts the creep should adopt.
  * @returns {number} best.carry - The number of CARRY parts the creep should adopt.
  * @returns {number} best.move - The number of MOVE parts the creep should adopt.
  */
-module.exports.calculateOptimalBodyPartSet = function(spawn, distance) {
+module.exports.calculateOptimalBodyPartSet = function(spawn, distance, upgradeController = true) {
     let energyCapacity = spawn.room.energyCapacityAvailable;
     
     var best = {efficiency:0, work:0, carry:0, move:0};
@@ -37,15 +39,16 @@ module.exports.calculateOptimalBodyPartSet = function(spawn, distance) {
             let move = (energyCapacity - work*BODYPART_COST[WORK] - carry*BODYPART_COST[CARRY]) / BODYPART_COST[MOVE];
             //console.log(work, carry, move);
             let timeToGetThere = Math.ceil(work / move) * distance;
-            let timeToMine = Math.ceil(carry*CARRY_CAPACITY / work*HARVEST_POWER);
+            let timeToMine = Math.ceil((carry*CARRY_CAPACITY) / (work*HARVEST_POWER));
             let timeToGetBack = Math.ceil((work + carry) / move) * distance;
+            let timeToTransfer = upgradeController ? carry*UPGRADE_CONTROLLER_POWER : 1;
             //console.log(work, carry, move, timeToGetThere, timeToMine, timeToGetBack);
             let totalTime = timeToGetThere + timeToMine + timeToGetBack;
             let energyMined = carry*CARRY_CAPACITY;
             let efficiency = energyMined / totalTime;
             //console.log(work, carry, move, totalTime, energyMined, efficiency);
             if (efficiency > best['efficiency'])
-                best = {efficiency:efficiency, work:work, carry:carry, move:move};
+                best = {work:work, carry:carry, move:move, efficiency:efficiency, totalTime:totalTime};
         }
     }
     //console.log(best);
